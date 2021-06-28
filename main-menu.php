64c899dcebddb6dec195b34b3d4c7716ce33e7a9
Larissa Almeida <larissa-oliveira42@outlook.com>
2021-06-28 00:42:58 -0300

    versao atualizada do site

---
+++ main-menu.php
@@ -0,0 +1,36 @@
+<header class="container-fluid">
+        <label>
+            <input type='checkbox'>
+            <span class='menu'>
+                <span class='hamburger'></span>
+            </span>
+            <ul>
+                <li>
+                  <a href='#' style="text-decoration: none;" class="menu-item">home</a>
+                </li>
+                <li>
+                   <a href='#' style="text-decoration: none;" class="menu-item">sobre</a>
+                </li>
+                <li>
+                    <a href='#' style="text-decoration: none;" class="menu-item">métodos</a>
+                </li>
+                <li>
+                    <a href='#' style="text-decoration: none;" class="menu-item">serviços</a>
+                </li>
+                <li>
+                   <a href='#' style="text-decoration: none;" class="menu-item">time</a>
+                </li>
+                <li>
+                    <a href='#' style="text-decoration: none;" class="menu-item">contato</a>
+                </li>
+            </ul>
+        </label>        
+
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/banner-topo.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+        </div>
+    </header>
