7dd0baee3d95801f81ccf6cb361b1a5988a5c126
Larissa Almeida <larissa-oliveira42@outlook.com>
2021-06-29 21:43:05 -0300

    mais uma atualização

--- js/team-carrousel.js
+++ js/team-carrousel.js
@@ -3,7 +3,18 @@ $(document).ready(function(){
         dots: true,
         infinite: false,
         speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
     });
   });
